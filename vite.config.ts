b78c0bd85edb5b3fe639fd58d989970a776ba951
itaym-intel <itay.mevorach@intel.com>
2026-02-22 01:46:31 -0800

    gh action deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.GITHUB_PAGES === 'true' ? '/rogue-like-black-jack/' : '/',
   root: '.',
   publicDir: 'public',
   build: {
